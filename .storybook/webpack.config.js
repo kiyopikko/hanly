632caedea902ca598ed88df86a0b9d478d5bf27e
Kiyohiko Heima <piyo326@gmail.com>
2019-06-23 16:30:23 +0900

    snap

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+
+module.exports = async ({ config, mode }) => {
+  const additionalRules = [
+    {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader']
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg|webp)$/,
+      loader: 'url-loader',
+      query: {
+        limit: 1000,
+        name: 'img/[name].[hash:7].[ext]'
+      }
+    },
+    {
+      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+      loader: 'url-loader',
+      query: {
+        limit: 1000,
+        name: 'fonts/[name].[hash:7].[ext]'
+      }
+    }
+  ]
+
+  config.module.rules = config.module.rules.concat(additionalRules)
+  return config
+}
---
+++ components/BaseButton.story.js
@@ -0,0 +1,22 @@
+import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
+import centered from '@storybook/addon-centered'
+import BaseButton from './BaseButton'
+
+const testObj = {
+  components: { BaseButton },
+  methods: {
+    change: action('change!')
+  }
+}
+
+storiesOf('BaseButton', module)
+  .addDecorator(centered)
+  .add('default', () => ({
+    ...testObj,
+    template: `<BaseButton @myclick="change">テスト</BaseButton>`
+  }))
+  .add('red', () => ({
+    ...testObj,
+    template: `<BaseButton @myclick="change" clr="red">テスト</BaseButton>`
+  }))
