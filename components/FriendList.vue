45c882fea6af83a7e7e17ab1aa7cb1b64d9847db
Kiyohiko Heima <piyo326@gmail.com>
2019-07-04 15:40:48 +0900

    Update components for pages

--- components/FriendList.vue
+++ components/FriendList.vue
@@ -3,10 +3,12 @@
     <FriendItem
       v-for="(item, i) in list"
       :key="i"
+      :index="i"
       :to="`${path}${item.id}`"
       :nickname="item.nickname"
       :date="item.date"
-      :img="item.img"
+      :img="item.img || ''"
+      :icon-placeholder="getPlaceholder(i)"
     />
   </div>
 </template>
@@ -15,6 +17,9 @@
 import FriendItem from './FriendItem'
 
 export default {
+  components: {
+    FriendItem
+  },
   props: {
     list: {
       type: Array,
@@ -25,8 +30,14 @@ export default {
       required: true
     }
   },
-  components: {
-    FriendItem
+  methods: {
+    getPlaceholder(i) {
+      return i % 3 === 0
+        ? 'linear-gradient(141.16deg, #F686AB 11.81%, #FFE177 82.38%)'
+        : i % 3 === 1
+        ? 'linear-gradient(141.16deg, #B7F4EF 11.81%, #E27EA0 85.05%)'
+        : 'linear-gradient(141.16deg, #FFE073 11.81%, #AFFDF6 85.05%)'
+    }
   }
 }
 </script>
